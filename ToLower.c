502d71c3f608523f5873ba2f5a028d1cef82df57
Tse Wang Pok Jerry 3036065642 <126223772+JerryTseee@users.noreply.github.com>
2023-12-13 21:07:16 +0800

    Create ToLower.c

---
+++ ToLower.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+// Task2. Build the toLower() function here.
+
+void toLower(char a[]) 
+{
+    // To be implemented by you.
+    for (int i = 0; i < strlen(a); i++)
+    {
+        if (a[i] >= 'A' && a[i] <= 'Z')
+        {
+            a[i] = a[i] + 'a' - 'A';//convert the upper case to lower case
+        }
+    }
+}
+int main() {
+    char input[100];//input itself already is a pointer
+    // Task 1. Read in user input to the char array input.
+    scanf("%s", input);
+    // Task 3. Call the toLower function.
+    toLower(input);
+    printf("%s", input);
+    return 0;
+}
